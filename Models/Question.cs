58b6ae50de05e2c08f1199a1d1c5d2746d8d9f49
Daniel Williams <dxw994@student.bham.ac.uk>
2023-02-18 17:58:46 +0000

    Questions are now json objects

--- Models/Question.cs
+++ Models/Question.cs
@@ -1,11 +1,11 @@
-﻿namespace PhishSceptic.Utilities
+﻿namespace PhishSceptic.Models
 {
     public class Question
     {
         public string question { get; set; }
-        public float weight { get; set; }
         public string description { get; set; }
+        public float weight { get; set; }
 
-        public bool? isCorrect { get; set; }
+        public bool? isYes { get; set; }
     }
 }
---
+++ Models/QuestionBank.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Http.Json;
+
+namespace PhishSceptic.Models
+{
+    public class QuestionBank
+    {
+        //use queue?
+        private Question[]? questions;
+        private readonly HttpClient _httpClient;
+
+        public QuestionBank(HttpClient http)
+        {
+            _httpClient = http;
+        }
+
+        public async Task LoadQuestions()
+        {
+            try
+            {
+                questions = await _httpClient.GetFromJsonAsync<Question[]>("data/questions.json");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+        }
+
+        public Question[] GetQuestions()
+        {
+            if (questions!=null)
+            {
+                return questions;
+            }
+            throw new Exception("Questions object is null");
+        }
+    }
+}
