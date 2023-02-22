f3232f6766bee086fd77584928a67c5b057885b2
Daniel Williams <dxw994@student.bham.ac.uk>
2023-02-22 21:06:17 +0000

    Added: word category tabs

--- Models/WordCategories.cs
+++ Models/WordCategories.cs
@@ -2,6 +2,11 @@
 {
     public class WordCategories
     {
+        //public string[] phishing { get; set; }
         public string[] urgent { get; set; }
+        public string[] reward { get; set; }
+        public string[] threat { get; set; }
+        public string[] misspelling { get; set; }
+
     }
 }
