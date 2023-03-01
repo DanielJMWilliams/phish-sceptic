3eabe4c83eb801a23d2981fb3d93fada059e0bb0
Daniel Williams <dxw994@student.bham.ac.uk>
2023-03-01 16:54:09 +0000

    WIP: multicolour highlighter. need to split word categories into regex groups

---
+++ PhishSceptic/Client/Components/MultiHighlighter.razor.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using MudBlazor;
+using MudBlazor.Components.Highlighter;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+
+namespace PhishSceptic.Client.Components
+{
+    public partial class MultiHighlighter : MudComponentBase
+    {
+
+        private Memory<string> _fragments;
+        private string _regex;
+
+        private string[] _regexes;
+
+        [Parameter] public string[] Colors { get; set; }
+
+        /// <summary>
+        /// The whole text in which a fragment will be highlighted
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.Highlighter.Behavior)]
+        public string Text { get; set; }
+
+        /// <summary>
+        /// The fragment of text to be highlighted
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.Highlighter.Behavior)]
+        public string HighlightedText { get; set; }
+
+        /// <summary>
+        /// The fragments of text to be highlighted
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.Highlighter.Behavior)]
+        public IEnumerable<string> HighlightedTexts { get; set; }
+
+        /// <summary>
+        /// Whether or not the highlighted text is case sensitive
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.Highlighter.Behavior)]
+        public bool CaseSensitive { get; set; }
+
+        /// <summary>
+        /// If true, highlights the text until the next regex boundary
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.Highlighter.Behavior)]
+        public bool UntilNextBoundary { get; set; }
+
+        //TODO
+        //Accept regex highlightings
+        // [Parameter] public bool IsRegex { get; set; }
+
+        protected override void OnParametersSet()
+        {
+            //would have to have multiple regexes
+            _fragments = Splitter.GetFragments(Text, HighlightedText, HighlightedTexts, out _regex, CaseSensitive, UntilNextBoundary);
+            Console.WriteLine(_regex);
+            _regexes = _regex.Split("|");
+            for (int i= 0; i < _regexes.Length; i++)
+            {
+                _regexes[i] = _regexes[i].Trim(new Char[] { '(', '?', ':', ')' });
+            }
+        }
+
+        /*
+        protected override void BuildRenderTree(RenderTreeBuilder __builder)
+        {
+            Span<string> span = _fragments.Span;
+            foreach (string text in span)
+            {
+                if (!string.IsNullOrWhiteSpace(_regex) && Regex.IsMatch(text, _regex, (!CaseSensitive) ? RegexOptions.IgnoreCase : RegexOptions.None))
+                {
+                    __builder.OpenElement(0, "mark");
+                    __builder.AddAttribute(1, "class", base.Class);
+                    __builder.AddAttribute(2, "style", base.Style);
+                    __builder.AddMultipleAttributes(3, RuntimeHelpers.TypeCheck((IEnumerable<KeyValuePair<string, object>>)base.UserAttributes));
+                    __builder.AddContent(4, text);
+                    __builder.CloseElement();
+                }
+                else
+                {
+                    __builder.AddContent(5, text);
+                }
+            }
+        }
+        */
+
+    }
+}
---
+++ PhishSceptic/Client/Components/UrgencyInspector.razor.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Components;
+using PhishSceptic.Client.Models;
+using PhishSceptic.Client.Utilities;
+using System.Net.Http.Json;
+
+namespace PhishSceptic.Client.Components
+{
+    public partial class UrgencyInspector
+    {
+        [Inject] HttpClient HttpClient { get; set; }
+        [Parameter] public EmailAnalyser emailAnalyser { get; set; }
+
+        private string _emailBody = "";
+        private string _emailTitle = "";
+
+        private string[] urgentWords;
+        private string[] threatWords;
+        private string[] rewardWords;
+        private string[] misspeltWords;
+        private string[] allSuspiciousWords;
+
+        private string[] _colors = { "mud-theme-info", "mud-theme-danger", "mud-theme-warning", "mud-theme-error" };
+        //private string[] phishingWords;
+
+        protected async override Task OnInitializedAsync()
+        {
+            await LoadWordCategories();
+            if (wordCategories != null)
+            {
+                urgentWords = wordCategories.urgent;
+                threatWords = wordCategories.threat;
+                rewardWords = wordCategories.reward;
+                misspeltWords = wordCategories.misspelling;
+                allSuspiciousWords = urgentWords.Concat(threatWords).Concat(rewardWords).Concat(misspeltWords).ToArray();
+
+            }
+
+        }
+
+        protected async override Task OnParametersSetAsync()
+        {
+            _emailBody = emailAnalyser.GetEmailBody();
+            _emailBody = EmailAnalyser.StripOutLinks(_emailBody);
+            _emailTitle = emailAnalyser.GetEmailTitle();
+
+            //urgentWords = wordCategories
+
+        }
+
+        private WordCategories? wordCategories;
+
+        public async Task LoadWordCategories()
+        {
+            try
+            {
+                wordCategories = await HttpClient.GetFromJsonAsync<WordCategories>("data/word_categories.json");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+        }
+    }
+}
