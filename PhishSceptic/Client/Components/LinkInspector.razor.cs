4e7af0be7df6d41e440b85a5a15945b8eaa319f6
Daniel Williams <dxw994@student.bham.ac.uk>
2023-03-16 16:32:22 +0000

    refactored link inspector

--- PhishSceptic/Client/Components/LinkInspector.razor.cs
+++ PhishSceptic/Client/Components/LinkInspector.razor.cs
@@ -24,8 +24,6 @@ namespace PhishSceptic.Client.Components
         private List<string> domains;
         private List<string> shortenedDomains;
         private List<string> urlsContainingAnchors;
-        private Color[] domainChipColors;
-        private string[] domainChipIcons;
 
         protected async override Task OnParametersSetAsync()
         {
@@ -33,66 +31,7 @@ namespace PhishSceptic.Client.Components
             urlsContainingAnchors = emailAnalyser.GetUrlsContainingAnchors();
             domains = emailAnalyser.GetDistinctDomains();
             shortenedDomains = emailAnalyser.GetShortenedDomains();
-            domainChipColors = new Color[domains.Count()];
-            domainChipIcons = new string[domains.Count()];
-            for (int i = 0; i < domainChipColors.Length; i++)
-            {
-                domainChipColors[i] = Color.Warning;
-                domainChipIcons[i] = Icons.Material.Filled.WarningAmber;//Icons.Material.Filled.Error
-            }
         }
 
-        private async Task VerifyDomain(int domainIndex)
-        {
-
-            var parameters = new DialogParameters();
-            parameters.Add("ContentText", "Verify that this domain really is what you think it is.");
-            parameters.Add("TextToVerify", domains[domainIndex]);
-
-            var options = new DialogOptions() { CloseButton = true, MaxWidth = MaxWidth.ExtraSmall };
-
-            var dialog = await DialogService.ShowAsync<InspectionDialog>("Verify Domain", parameters, options);
-
-
-            var result = await dialog.Result;
-
-            if (result.Canceled)
-            {
-                return;
-            }
-
-            int rep = await vtService.CheckReputation(domains[domainIndex]);
-            if (rep == -1)
-            {
-                //unscanned
-                domainChipColors[domainIndex] = Color.Warning;
-                domainChipIcons[domainIndex] = Icons.Material.Filled.WarningAmber;
-            }
-            else if (rep > 0)
-            {
-                //scanned and bad
-                domainChipColors[domainIndex] = Color.Error;
-                domainChipIcons[domainIndex] = Icons.Material.Filled.Dangerous;
-                Snackbar.Add("VirusTotal flagged this domain as suspicious.", Severity.Error);
-                warningService.AddWarning("dodgy link: " + domains[domainIndex]);
-            }
-            else if(rep == 0)
-            {
-                //scanned and fine
-                domainChipColors[domainIndex] = Color.Info;
-                domainChipIcons[domainIndex] = Icons.Material.Filled.Check;
-            }
-            else if (rep == -2)
-            {
-                //no response, rate limited
-                domainChipColors[domainIndex] = Color.Warning;
-                domainChipIcons[domainIndex] = "";
-                Snackbar.Add("VirusTotal Rate Limit hit. Wait a few minutes and try again.", Severity.Info);
-            }
-
-
-
-            StateHasChanged();
-        }
     }
 }
